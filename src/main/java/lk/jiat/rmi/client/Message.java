23f37f0f51dc0e088e7ef82c61660f9820db53a4
kalhara05 <kalhara139@gmail.com>
2026-05-25 23:14:06 +0530

    Add getData to Message RMI interface
    Introduce a new remote method getData() on the Message RMI interface to return a Data model instance; added import for lk.jiat.rmi.model.Data. The method declares RemoteException to comply with java.rmi.Remote requirements so clients can fetch structured data from the server.

--- src/main/java/lk/jiat/rmi/client/Message.java
+++ src/main/java/lk/jiat/rmi/client/Message.java
@@ -1,8 +1,10 @@
 package lk.jiat.rmi.client;
 
+import lk.jiat.rmi.model.Data;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Message extends Remote {
     public String hello() throws RemoteException;
+    public Data getData() throws RemoteException;
 }
