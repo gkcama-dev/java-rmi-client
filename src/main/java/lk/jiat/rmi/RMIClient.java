03378e968f4a5e600ca22900cd9ec6a423220546
kalhara05 <kalhara139@gmail.com>
2026-05-26 00:54:02 +0530

    Use UserService in RMI client and print users
    Lookup and use the remote UserService from the RMI registry: added imports for UserService and User, performed registry.lookup("user_service"), added three test users, and iterated over getAllUsers() to print user details. Also added separator printlns for clearer console output. These changes extend the client to exercise and display the remote user API for testing/demo purposes.

--- src/main/java/lk/jiat/rmi/RMIClient.java
+++ src/main/java/lk/jiat/rmi/RMIClient.java
@@ -1,8 +1,11 @@
 package lk.jiat.rmi;
 
 import lk.jiat.rmi.client.Message;
+import lk.jiat.rmi.client.UserService;
 import lk.jiat.rmi.model.Data;
+import lk.jiat.rmi.model.User;
 
+import java.rmi.Remote;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
@@ -19,12 +22,27 @@ public class RMIClient {
             Message message = (Message) registry.lookup("message_service");
             String msg = message.hello();
 
+            System.out.println("................");
             System.out.println(msg);
+            System.out.println("................");
+            System.out.println("................");
 
             Data data = message.getData();
 
             System.out.println(data.getId());
             System.out.println(data.getName());
+            System.out.println("................");
+            System.out.println("................");
+
+            UserService userService = (UserService) registry.lookup("user_service");
+            userService.addUser(1,new User(1,"Jhon","Colombo","jhon@gmail.com"));
+            userService.addUser(2,new User(2,"Kasun","Colombo","kasun@gmail.com"));
+            userService.addUser(3,new User(3,"Snow","Colombo","snow@gmail.com"));
+
+            userService.getAllUsers().forEach(user -> {
+                System.out.println(user.getId() + " " + user.getName()+" "+ user.getAddress() + " " + user.getEmail());
+                System.out.println(".................................");
+            });
 
         } catch (Exception e) {
             throw new RuntimeException(e);
