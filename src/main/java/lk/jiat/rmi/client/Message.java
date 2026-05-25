82833ee1e9986c355b13d8f897e91041908e3f66
kalhara05 <kalhara139@gmail.com>
2026-05-26 00:53:26 +0530

    Add getResult to Message; update IDE metadata
    Add a new remote method getResult(int num1, int num2) to src/main/java/lk/jiat/rmi/client/Message.java to expose a result-calculation via RMI. Corresponding compiled class files in target/classes were updated. Also update .idea/workspace.xml to record recent changelist entries, recents, and minor IDE metadata/work item updates.

--- src/main/java/lk/jiat/rmi/client/Message.java
+++ src/main/java/lk/jiat/rmi/client/Message.java
@@ -7,4 +7,5 @@ import java.rmi.RemoteException;
 public interface Message extends Remote {
     public String hello() throws RemoteException;
     public Data getData() throws RemoteException;
+    public String getResult(int num1, int num2) throws RemoteException;
 }
